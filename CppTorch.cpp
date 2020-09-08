a08282a39843c93b57b178b470d2f67eab6cdf0a
neteraxe <neteraxe@126.com>
2020-09-08 22:15:41 +0800

    init first

---
+++ CppTorch.cpp
@@ -0,0 +1,10 @@
+﻿// CppTorch.cpp : Defines the entry point for the application.
+//
+
+#include <torch/torch.h>
+#include <iostream>
+
+int main() {
+	torch::Tensor tensor = torch::rand({ 2, 3, 8, 9 });
+	std::cout << tensor << std::endl;
+}
